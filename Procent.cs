6a9894315d8d41b9343a4b929be369ed7fcff9b8
Natalliax <Natalliax@NATALLIAX>
2023-07-06 10:03:56 +0300

    Добавьте файлы проекта.

---
+++ Procent.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB3_1
+{
+
+    public class Procent
+    {
+        public string? name; //название вычисления
+        public double fraht = 1000;
+        public double SumTonn = 35;
+        public double Vyr = 35000;
+        public double MaxTonn = 45;
+        public double MaxVyr = 45000;
+        public double procent = 0;
+
+
+        public Procent(string Name, double Fraht, double sumTonn, double vyr, int maxTonn, double maxVyr)
+        {
+            this.name = Name;
+            this.MaxTonn = maxTonn;
+            this.fraht = Fraht;
+            this.SumTonn = sumTonn;
+            this.MaxVyr = maxVyr;
+            this.Vyr = vyr;
+
+        }
+
+
+        public double ProcentVyr()
+            // метод для определения процентной выручки от максимально возможной
+            //(суммарную грузоподъемность умножить на стоимость перевозки 1
+            //тонны)
+        {
+         
+            for (int i = 0; i < MaxTonn; i++)
+            {
+                procent = (Vyr * 100)/MaxVyr;
+               
+            }
+            return procent;
+        }
+        public override string ToString()
+
+        {
+            return $"Название  {name}\n" +
+                   $"Сумма тонн {SumTonn}\n" +
+                   $"Выручка {Vyr}\n" +
+                   $"Максимально тонн {MaxTonn}\n" +
+                   $"Максимальная выручка {MaxVyr}\n";
+        }
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//Матюшенко Наталья 20331гр.
+using System.Xml.Linq;
+
+namespace LAB3_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Procent procent1 = new Procent("Виатранс", 1000, 35, 35000, 45, 45000);
+            Console.WriteLine(procent1);
+            Console.WriteLine(procent1.ProcentVyr());
+
+        }
+
+
+
+    }
+
+       
+}
